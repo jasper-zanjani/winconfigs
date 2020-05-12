b6dab2d09876bb1075de2ea3fbe806b2a504f44f
Jasper Zanjani <jasper.zanjani@gmail.com>
2020-05-12 09:24:22 -0400

    Updating prompt

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,5 +3,12 @@ function zgip {
   Get-AzPublicIpAddress -Name "($VMName)-ip" | select -ExpandProperty IpAddress
 }
 
+function prompt {
+  Write-Host -ForegroundColor DarkGray ('[PS] ') -NoNewLine
+  Write-Host -ForegroundColor Yellow (Get-Item $pwd).BaseName -NoNewLine
+  Write-Host '>' -NoNewLine
+  return " "
+}
+
 New-Alias zicm Invoke-AzVMRunCommand
 New-Alias gcr Get-Credential 
